4f1f35025cc6a3cdfb161f8482167e132086cd46
Nick Campbell <nicholas.j.campbell@gmail.com>
2011-03-28 01:25:47 -0400

    Async tests and a little async example...also includes times so you can get an idea of how long it takes to run.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

---
+++ examples/async.js
@@ -0,0 +1,20 @@
+var bcrypt = require('./bcrypt');
+
+var start = Date.now();
+bcrypt.gen_salt(10, function(err, salt) {
+    console.log('salt: ' + salt);
+    console.log('salt cb end: ' + (Date.now() - start) + 'ms');
+    bcrypt.encrypt('test', salt, function(err, crypted) {
+        console.log('crypted: ' + crypted);
+        console.log('crypted cb end: ' + (Date.now() - start) + 'ms');
+        bcrypt.compare('test', crypted, function(err, res) {
+            console.log('compared true: ' + res);
+            console.log('compared true cb end: ' + (Date.now() - start) + 'ms');
+        });
+        bcrypt.compare('bacon', crypted, function(err, res) {
+            console.log('compared false: ' + res);
+            console.log('compared false cb end: ' + (Date.now() - start) + 'ms');
+        });
+    });
+})
+console.log('end: ' + (Date.now() - start) + 'ms');
---
+++ test/async.js
@@ -0,0 +1,87 @@
+var testCase = require('nodeunit').testCase,
+    bcrypt = require('../bcrypt');
+
+module.exports = testCase({
+        test_salt_length: function(assert) {
+            assert.expect(1);
+            bcrypt.gen_salt(10, function(err, salt) {
+                assert.equals(29, salt.length, "Salt isn't the correct length.");
+                assert.done();
+            });
+        },
+    test_salt_no_params: function(assert) {
+        assert.throws(function() {bcrypt.gen_salt();}, "Should throw an Error. gen_salt requires # of rounds.");
+        assert.done();
+    },
+    test_salt_rounds_is_string_number: function(assert) {
+        assert.throws(function() {bcrypt.gen_salt('10');}, "Should throw an Error. No params.");
+        assert.done();
+    },
+    test_salt_rounds_is_NaN: function(assert) {
+        assert.throws(function() {bcrypt.gen_salt('b');}, "Should throw an Error. gen_salt requires rounds to be a number.");
+        assert.done();
+    },
+    test_encrypt: function(assert) {
+        assert.expect(1);
+        bcrypt.gen_salt(10, function(err, salt) {
+            bcrypt.encrypt('password', salt, function(err, res) {
+                assert.ok(res, "Shouldn't throw an Error.");
+                assert.done();
+            });
+        });
+    },
+    test_encrypt_no_params: function(assert) {
+        assert.throws(function() {bcrypt.encrypt();}, "Should throw an Error. No Params.");
+        assert.done();
+    },
+    test_encrypt_one_param: function(assert) {
+        assert.throws(function() {bcrypt.encrypt('password');}, "Should throw an Error. No salt.");
+        assert.done();
+    },
+    test_encrypt_not_hash_str: function(assert) {
+        assert.throws(function() {bcrypt.encrypt('password', 1);}, "Should throw an Error. hash should be a string.");
+        assert.done();
+    },
+    test_verify_salt: function(assert) {
+        assert.expect(2);
+        bcrypt.gen_salt(10, function(err, salt) {
+            var split_salt = salt.split('$');
+            assert.ok(split_salt[1], '2a');
+            assert.ok(split_salt[2], '10');
+            assert.done();
+        });
+    },
+    test_verify_salt_min_rounds: function(assert) {
+        assert.expect(2);
+        bcrypt.gen_salt(1, function(err, salt) {
+            var split_salt = salt.split('$');
+            assert.ok(split_salt[1], '2a');
+            assert.ok(split_salt[2], '4');
+            assert.done();
+        });
+    },
+    test_verify_salt_max_rounds: function(assert) {
+        assert.expect(2);
+        bcrypt.gen_salt(100, function(err, salt) {
+            var split_salt = salt.split('$');
+            assert.ok(split_salt[1], '2a');
+            assert.ok(split_salt[2], '31');
+            assert.done();
+        });
+    },
+    test_hash_compare: function(assert) {
+        assert.expect(3);
+        bcrypt.gen_salt(10, function(err, salt) {
+            assert.equals(29, salt.length, "Salt isn't the correct length.");
+            bcrypt.encrypt("test", salt, function(err, hash) {
+                bcrypt.compare("test", hash, function(err, res) {
+                    assert.ok(res, "These hashes should be equal.");
+                    bcrypt.compare("blah", hash, function(err, res) {
+                        assert.ok(!res, "These hashes should not be equal.");
+                        assert.done();
+                    });
+                });
+            });
+        });
+    }
+});
