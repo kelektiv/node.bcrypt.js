71cd6b3bad202719b0b442a39bfd1c986100483f
Roman Shtylman <shtylman@gmail.com>
2011-11-10 00:58:25 -0500

    add some jsdoc style documentation

--- bcrypt.js
+++ bcrypt.js
@@ -5,7 +5,10 @@ try {
   var bindings = require('./build/Release/bcrypt_lib');
 }
 
-// return the salt
+/// generate a salt (sync)
+/// @param {Number} [rounds] number of rounds (default 10)
+/// @param {Number} [seed_length] number of random bytes (default 20)
+/// @return {String} salt
 module.exports.gen_salt_sync = function(rounds, seed_length) {
 
     // default 10 rounds
@@ -25,7 +28,10 @@ module.exports.gen_salt_sync = function(rounds, seed_length) {
     return bindings.gen_salt_sync(rounds, seed_length);
 };
 
-// cb (err, sault)
+/// generate a salt
+/// @param {Number} [rounds] number of rounds (default 10)
+/// @param {Number} [seed_length] number of random bytes (default 20)
+/// @param {Function} cb callback(err, salt)
 module.exports.gen_salt = function(rounds, seed_length, cb) {
 
     // if callback is first argument, then use defaults for others
@@ -62,6 +68,10 @@ module.exports.gen_salt = function(rounds, seed_length, cb) {
     return bindings.gen_salt(rounds, seed_length, cb);
 };
 
+/// hash data using a salt
+/// @param {String} data the data to encrypt
+/// @param {String} salt the salt to use when hashing
+/// @return {String} hash
 module.exports.encrypt_sync = function(data, salt) {
     if (!data || !salt) {
         throw new Error('data and salt arguments required');
@@ -72,7 +82,10 @@ module.exports.encrypt_sync = function(data, salt) {
     return bindings.encrypt_sync(data, salt);
 };
 
-// cb (err, hash)
+/// hash data using a salt
+/// @param {String} data the data to encrypt
+/// @param {String} salt the salt to use when hashing
+/// @param {Function} cb callback(err, hash)
 module.exports.encrypt = function(data, salt, cb) {
     if (!data || !salt) {
         throw new Error('data and salt arguments required');
@@ -89,7 +102,10 @@ module.exports.encrypt = function(data, salt, cb) {
     return bindings.encrypt(data, salt, cb);
 };
 
-// return true if matched
+/// compare raw data to hash
+/// @param {String} data the data to hash and compare
+/// @param {String} hash expected hash
+/// @return {bool} true if hashed data matches hash
 module.exports.compare_sync = function(data, hash) {
     if (!data || !hash) {
         throw new Error('data and hash arguments required');
@@ -100,7 +116,10 @@ module.exports.compare_sync = function(data, hash) {
     return bindings.compare_sync(data, hash);
 };
 
-// cb (err, same)
+/// compare raw data to hash
+/// @param {String} data the data to hash and compare
+/// @param {String} hash expected hash
+/// @param {Function} cb callback(err, matched) - matched is true if hashed data matches hash
 module.exports.compare = function(data, hash, cb) {
     if (!data || !hash) {
         throw new Error('data and hash arguments required');
@@ -117,6 +136,8 @@ module.exports.compare = function(data, hash, cb) {
     return bindings.compare(data, hash, cb);
 };
 
+/// @param {String} hash extract rounds from this hash
+/// @return {Number} the number of rounds used to encrypt a given hash
 module.exports.get_rounds = function(hash) {
     if (!hash) {
         throw new Error('hash argument required');
