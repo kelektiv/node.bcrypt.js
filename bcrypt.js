a5ecd97385e39b317fb4fb725815818aaabc6003
Kevin Smyth <kevin.m.smyth@gmail.com>
2016-09-02 19:43:51 -0500

    Add node-pre-gyp support for prebuilt binaries (#278)

--- bcrypt.js
+++ bcrypt.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var bindings = require('bindings')('bcrypt_lib');
+var binary = require('node-pre-gyp');
+var path = require('path');
+var binding_path = binary.find(path.resolve(path.join(__dirname, './package.json')));
+var bindings = require(binding_path);
+
 var crypto = require('crypto');
 
 /// generate a salt (sync)
