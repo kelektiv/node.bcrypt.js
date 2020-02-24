db06d9b169f9ef23bfc7fd0a07d8c01e75f99e70
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2020-02-24 22:46:32 +0530

    Prepare for uploading releases from inside docker

--- test_alpine.sh
+++ test_alpine.sh
@@ -6,3 +6,7 @@ echo "Running on $(node -v)"
 
 apk add make g++ python
 npm test --unsafe-perm
+
+./node_modules/.bin/node-pre-gyp configure
+./node_modules/.bin/node-pre-gyp build
+./node_modules/.bin/node-pre-gyp package
