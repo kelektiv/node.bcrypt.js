feb477c8702e460686c3473c31f69a2a19c7f446
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2021-02-20 18:54:53 +0530

    Update node-pre-gyp to 1.0.0

--- bcrypt.js
+++ bcrypt.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var binary = require('node-pre-gyp');
+var nodePreGyp = require('@mapbox/node-pre-gyp');
 var path = require('path');
-var binding_path = binary.find(path.resolve(path.join(__dirname, './package.json')));
+var binding_path = nodePreGyp.find(path.resolve(path.join(__dirname, './package.json')));
 var bindings = require(binding_path);
 
 var crypto = require('crypto');
