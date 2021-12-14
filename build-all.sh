ea2a8fd256bc1682143841c9daed99436f92517e
Thom Nichols <95562+thom-nic@users.noreply.github.com>
2021-12-14 13:16:12 -0500

    .github: added github actions

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ue
 
 CLEAN=${CLEAN:-""}
-RUN_TESTS=${RUN_TESTS:-false}  # tests presently fail on arm :(
+RUN_TESTS=${RUN_TESTS:-true}
 
 if [ -n "$CLEAN" ]; then
   rm -rf build build-tmp*
@@ -14,24 +14,24 @@ npm run build
 
 # build for linux/x64:
 if [ ! -d prebuilds/linux-x64 ]; then
-  docker build -t voltserver/bcryptjs-linux-x64-builder .
-  CONTAINER=$(docker create voltserver/bcryptjs-linux-x64-builder)
+  docker build -t bcryptjs-linux-x64-builder -f Dockerfile-x64 --build-arg RUN_TESTS="$RUN_TESTS" .
+  CONTAINER=$(docker create bcryptjs-linux-x64-builder)
   docker cp "$CONTAINER:/usr/local/opt/bcrypt-js/prebuilds" .
   docker rm "$CONTAINER"
 fi
 
 # build for linux/arm32:
 if [ ! -d prebuilds/linux-arm ]; then
-  docker build -t voltserver/bcryptjs-linux-arm-builder -f Dockerfile-arm --build-arg RUN_TESTS="$RUN_TESTS" .
-  CONTAINER=$(docker create --platform linux/arm/v7 voltserver/bcryptjs-linux-arm-builder)
+  docker build -t bcryptjs-linux-arm-builder -f Dockerfile-arm --build-arg RUN_TESTS="$RUN_TESTS" .
+  CONTAINER=$(docker create --platform linux/arm/v7 bcryptjs-linux-arm-builder)
   docker cp "$CONTAINER:/usr/local/opt/bcrypt-js/prebuilds" .
   docker rm "$CONTAINER"
 fi
 
 # build for linux/arm64:
 if [ ! -d prebuilds/linux-arm64 ]; then
-  docker build -t voltserver/bcryptjs-linux-arm64-builder -f Dockerfile-arm64 --build-arg RUN_TESTS="$RUN_TESTS" .
-  CONTAINER=$(docker create --platform linux/arm64/v8 voltserver/bcryptjs-linux-arm64-builder)
+  docker build -t bcryptjs-linux-arm64-builder -f Dockerfile-arm64 --build-arg RUN_TESTS="$RUN_TESTS" .
+  CONTAINER=$(docker create --platform linux/arm64/v8 bcryptjs-linux-arm64-builder)
   docker cp "$CONTAINER:/usr/local/opt/bcrypt-js/prebuilds" .
   docker rm "$CONTAINER"
 fi
